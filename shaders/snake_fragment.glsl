28497e986a1c2aa030339c7f3abb81869e01bea3
Joona Perasto <joona.perasto@tuni.fi>
2019-04-16 03:04:27 +0300

    Add snake thickness variable and fade effect towards tail

--- shaders/snake_fragment.glsl
+++ shaders/snake_fragment.glsl
@@ -1,11 +1,21 @@
 varying highp vec3 normal;
 varying highp vec3 tail; // tail: x = pos, y = bulge, z = ? possibly change color by length
-uniform lowp vec3 albedo;
+uniform mediump float tailLength;
 
-const vec3 lightDir = vec3(.8, 0.4, .8);
+const vec3 lightDir = vec3(0.8, 0.4, 0.8);
+const vec3 baseColor = vec3(0.2, 0.7, 0.2);
+const vec3 texColor = vec3(0.4, 1.0, 0.3);
+const vec3 fadeColor = vec3(0.5, 0.3, 0.2);
 
 void main(void)
 {
-   float lighting = step(.2, dot(normal, lightDir));
-   gl_FragColor = vec4(.7*albedo + .3*albedo*lighting, 1.0);
+   float lightMult = step(.2, dot(normal, lightDir));
+
+   float texFade = step(0, .4*sin(tail.x*24 + cos(tail.z*2)*4) + .7*sin(tail.x*48));
+   float lengthFade = 0.7*tail.x/tailLength;
+
+   vec3 tex = mix(baseColor, texColor, texFade);
+   vec3 albedo = mix(tex, fadeColor, lengthFade);
+
+   gl_FragColor = vec4(.7*albedo + .3*albedo*lightMult, 1.0);
 }
--- shaders/snake_vertex.glsl
+++ shaders/snake_vertex.glsl
@@ -1,13 +1,35 @@
+#define PI 3.1415926535897932384626433832795
+
 attribute highp vec4 aVertex;
 attribute highp vec3 aNormal;
 attribute highp vec3 aTail;
+
 uniform highp mat4 mvpMatrix;
+uniform mediump float tailLength;
+
 varying highp vec3 normal;
 varying highp vec3 tail;
 
+
+const float THICKNESS = 0.05f;
+const float HEAD_LENGTH = 0.2f;
+const float TAIL_END_LENGTH = 0.25f;
+
+
+float calcThickness(float pos)
+{
+    if (pos < HEAD_LENGTH)
+        return sin((pos/HEAD_LENGTH) * PI * 0.6f) * 0.06f;
+    else if (pos > tailLength - TAIL_END_LENGTH)
+        return 0.5 * THICKNESS * sin((1+pos/HEAD_LENGTH) * PI/2);
+    return THICKNESS * (1-pos/tailLength*0.5);
+}
+
 void main(void)
 {
-   gl_Position = mvpMatrix * aVertex; // TODO: implement bulge from food
-   normal = aNormal;
-   tail = aTail;
+    float thickness = calcThickness(aTail.x);
+    vec4 newPos = vec4(aVertex.xyz + aNormal * thickness + vec3(0, 0, thickness/2), aVertex.w);
+    gl_Position = mvpMatrix * newPos; // TODO: implement bulge from food
+    normal = aNormal;
+    tail = aTail;
 }
